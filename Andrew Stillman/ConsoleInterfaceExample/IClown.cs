7e104894cda1adefa6159c8b6076345e6f5295f8
Dmitry_Meshkov <dm_91@bk.ru>
2020-12-28 21:14:57 +0300

    console onterface example

---
+++ Andrew Stillman/ConsoleInterfaceExample/IClown.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleInterfaceExample
+{
+    interface IClown
+    {
+        string FunnyThingIHave { get; }
+        void Honk();
+    }
+}
---
+++ Andrew Stillman/ConsoleInterfaceExample/TallGuy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleInterfaceExample
+{
+    class TallGuy : IClown
+    {
+        public string Name { get; set; }
+        public int Height { get; set; }
+
+        public string FunnyThingIHave => "Big Shoes";
+
+        public void Honk()
+        {
+            Console.WriteLine("Honk Honk");
+        }
+
+        public void TalkAboutYourself()
+        {
+            Console.WriteLine($"My name is {Name} and I'm {Height} inches tall");
+        }
+    }
+}
